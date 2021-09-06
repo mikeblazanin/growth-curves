3cce34e42faa02e3471f32d962c4e1c627cafdfb
mikeblazanin <mikeblazanin@gmail.com>
2021-09-06 16:47:09 -0400

    Added summarization & density plots of run2

--- paper_simulations/paper_simulations.R
+++ paper_simulations/paper_simulations.R
@@ -1231,10 +1231,10 @@ if (glob_make_statplots) {
     NULL
 }
 
-##Run 2: bact variants ----
+##Run 2: bact variants simulation & summarization ----
 run2 <- run_sims_filewrapper(name = "run2",
                      u_Svals = signif(2**c(-5, -5.5, -6, -6.5, -7), 2),
-                     k_Svals = 10**c(6, 7, 8, 9, 10),
+                     k_Svals = 10**c(7, 8, 9, 10, 11),
                      avals = 10**c(-8, -9, -10, -11, -12),
                      tauvals = signif(10**c(1.4, 1.8), 2),
                      bvals = signif(10**c(1.5, 2.5), 2),
@@ -1246,3 +1246,111 @@ run2 <- run_sims_filewrapper(name = "run2",
                      init_stepsize = 1,
                      print_info = TRUE,
                      read_file = glob_read_files)
+
+ybig2 <- run2[[1]]
+
+ybig2 <- group_by_at(ybig2, .vars = 1:17)
+
+#Summarize runs (using ifelse to handle potential non-equil conditions)
+y_summarized2 <- 
+  dplyr::summarize(
+    ybig2,
+    max_dens = max(Density[Pop == "B"]),
+    max_time = time[Pop == "B" & Density[Pop == "B"] == max_dens],
+    #Make references for finding extin point
+    extin_dens = 10**4,
+    #Technically this is the first index after extinction
+    extin_index =
+      ifelse(any(Pop == "B" & Density <= extin_dens & time >= max_time),
+             min(which(Pop == "B" & Density <= extin_dens & time >= max_time)),
+             NA),
+    extin_index_back1 =
+      ifelse(is.na(extin_index), NA,
+             which(Pop == "B")[match(extin_index, which(Pop == "B")) - 1]),
+    #use linear interpolation to find extin time
+    extin_time =
+      ifelse(is.na(extin_index), NA,
+             time[extin_index] - (Density[extin_index] - extin_dens)*
+               (time[extin_index] - time[extin_index_back1])/
+               (Density[extin_index] - Density[extin_index_back1])),
+    #make references for auc (here indices are within Pop == "B")
+    extin_index_winB =
+      ifelse(is.na(extin_time), NA, which(which(Pop == "B") == extin_index)),
+    extin_index_back1_winB =
+      ifelse(is.na(extin_time), NA, which(which(Pop == "B") == extin_index)-1),
+    extin_index_back2_winB =
+      ifelse(is.na(extin_time), NA, which(which(Pop == "B") == extin_index)-2),
+    #using trapezoid rule to find auc
+    auc =
+      ifelse(is.na(extin_time), NA,
+             #trapezoids of all intervals before one ending at extin time
+             (time[Pop == "B"][2] - time[Pop == "B"][1])/2 *
+               (Density[Pop == "B"][1] +
+                  2*sum(Density[Pop == "B"][2:extin_index_back2_winB]) +
+                  Density[Pop == "B"][extin_index_back1_winB]) +
+               #trapezoid that ends at extin time
+               (extin_time-time[extin_index_back1])/2 *
+               (Density[extin_index_back1] + extin_dens)),
+    phage_final = ifelse(equil[1], max(Density[Pop == "P"]), NA),
+    #make references for phage extin dens
+    phage_dens_y1 = ifelse(is.na(extin_time), NA,
+                           Density[max(which(Pop == "P" & time < extin_time))]),
+    phage_dens_y2 = ifelse(is.na(extin_time), NA,
+                           Density[which(Pop == "P" & time == time[extin_index])]),
+    phage_time_x1 = ifelse(is.na(extin_time), NA,
+                           time[max(which(Pop == "P" & time < extin_time))]),
+    phage_time_x2 = ifelse(is.na(extin_time), NA,
+                           time[which(Pop == "P" & time == time[extin_index])]),
+    #using linear interpolation to find phage dens at bact extinction
+    phage_extin = ifelse(is.na(extin_time), NA,
+                         phage_dens_y1 +
+                           (phage_dens_y2-phage_dens_y1)/(phage_time_x2-phage_time_x1)*
+                           (extin_time-phage_time_x1)),
+    # phage_r =
+    #   (log(phage_final)- log(init_S_dens[1]*init_moi[1]))/extin_time,
+    # phage_r_extin =
+    #   (log(phage_extin) - log(init_S_dens[1]*init_moi[1]))/extin_time,
+    phage_atmaxdens = Density[Pop == "P" & time == max_time],
+    maxdens_k_ratio = max_dens/k_S[1],
+    run_time = max(time),
+    equil = equil[1]
+  )
+
+#Drop unneeded columns
+y_summarized2 <- subset(y_summarized2,
+                        select = -c(extin_index, extin_index_back1,
+                                    extin_index_winB, extin_index_back1_winB,
+                                    extin_index_back2_winB, phage_dens_y1,
+                                    phage_dens_y2, phage_time_x1,
+                                    phage_time_x2))
+y_summarized2 <- as.data.frame(y_summarized2)
+
+#Run 2: density dynamics ----
+dir.create("./run2_noequil/", showWarnings = F)
+if (glob_make_curveplots) {
+  for (myrun in unique(ybig2$uniq_run[ybig2$equil == FALSE])) {
+    tiff(paste("./run2_noequil/", myrun, ".tiff", sep = ""),
+         width = 4, height = 4, units = "in", res = 200)
+    print(ggplot(data = ybig2[ybig2$uniq_run == myrun &
+                                ybig2$Pop %in% c("S", "I", "P"), ],
+                 aes(x = time, y = Density+1, color = Pop)) +
+            geom_line(lwd = 1.5, alpha = 0.5) +
+            scale_y_continuous(trans = "log10"))
+    dev.off()
+  }
+}
+
+dir.create("./run2_equil/", showWarnings = F)
+if (glob_make_curveplots) {
+  for (myrun in unique(ybig2$uniq_run[ybig2$equil == TRUE])) {
+    tiff(paste("./run2_equil/", myrun, ".tiff", sep = ""),
+         width = 4, height = 4, units = "in", res = 200)
+    print(ggplot(data = ybig2[ybig2$uniq_run == myrun &
+                                ybig2$Pop %in% c("S", "I", "P"), ],
+                 aes(x = time, y = Density+1, color = Pop)) +
+            geom_line(lwd = 1.5, alpha = 0.5) +
+            scale_y_continuous(trans = "log10"))
+    dev.off()
+  }
+}
+
