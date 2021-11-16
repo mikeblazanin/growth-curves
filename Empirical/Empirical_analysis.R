a81041f2e6d923aa6b25d1d160552856b3f97f7a
mikeblazanin <mikeblazanin@gmail.com>
2021-11-16 16:47:59 -0500

    Started analysis of empirical data

---
+++ Empirical/Empirical_analysis.R
@@ -0,0 +1,49 @@
+#Import functions ----
+library(growth.curves.pkg)
+library(ggplot2)
+
+#Read data ----
+trav_resis <- read.csv("./Empirical/trav-phage_resis_data.csv")
+
+gcdata_10_25 <- 
+  import_widemeasures("./Empirical/2021-10-25_Emma_Growth_Curve_Assay.csv",
+                    startrow = 30, startcol = 2,
+                    endrow = 126, endcol = 99)
+colnames(gcdata_10_25)[2] <- "Temp"
+
+gcdata_10_25_lng <- pivot_wide_longer(gcdata_10_25,
+                                   id_cols = c("Time", "Temp"))
+
+design_10_25 <- 
+  make_tidydesign(
+    nrows = 8, ncols = 12,
+    block_row_names = LETTERS[1:8],
+    block_col_names = 1:12,
+    wellnames_sep = "",
+    bacteria = make_designpattern(c("PF", "125ALE", "7xEGC", "7xACD",
+                                    "125ALA", "125CGE"),
+                                  rows = 2:7, cols = 2:4,
+                                  pattern = "111222333444555666",
+                                  byrow = TRUE),
+    bacteria = make_designpattern(c("7xCLD", "7xEGD",
+                                    "7xALA", "125CGA", "Blank"),
+                                  rows = 2:6, cols = 5:7,
+                                  pattern = "111222333444555",
+                                  byrow = TRUE),
+    bacteria = make_designpattern(c("PF", "125ALE", "7xEGC", "7xACD",
+                                    "125ALA", "125CGE"),
+                                  rows = 2:7, cols = 8:9,
+                                  pattern = "112233445566",
+                                  byrow = TRUE),
+    bacteria = make_designpattern(c("7xCLD", "7xEGD",
+                                    "7xALA", "125CGA", "Blank"),
+                                  rows = 2:6, cols = 10:11,
+                                  pattern = "1122334455",
+                                  byrow = TRUE),
+    phage = make_designpattern(c("Phage Added", "No Phage"),
+                               rows = 2:7, cols = 2:11,
+                               pattern = "111111222211111122221111112222111111222211100022001110002200",
+                               byrow = TRUE),
+  )
+                                  
+                                                              
