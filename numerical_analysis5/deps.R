c9610d001500962534cc13d1dbd6ad7fcfce30f5
Mike Blazanin <mikeblazanin@gmail.com>
2025-04-11 15:58:58 -0700

    added some unlisted dependencies; updated R version

---
+++ numerical_analysis5/deps.R
@@ -0,0 +1,6 @@
+#This file includes some packages which are dependencies for the main script
+# but which are suggested dependencies and not loaded with library(), so renv
+# fails to capture them with snapshot()
+
+library(GGally)
+library(ggforce)
